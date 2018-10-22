ca08f9b1f59a31840eb33f64e2f81e2ed17a5a40
edipetres <edipetres@gmail.com>
2018-10-22 10:22:06 +0200

    fix config import

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Axios from 'axios'
-import config from './config'
+const config = require('./config')
 
 Vue.config.productionTip = false
 Vue.prototype.$http = Axios.create({baseURL: process.env.NODE_ENV == 'development' ? config.apiUrl.dev : config.apiUrl.prod}); // using The Ternary Operator
