143023fda3696117f2ff161c969dc20a1bfc8352
edipetres <edipetres@gmail.com>
2018-10-23 14:04:13 +0200

    update hackernews home ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,31 @@
 <template>
-<center>
-  <div class="home">
-    
-    <table border="0" cellpadding="0" cellspacing="0" width="85%" bgcolor="#f6f6ef">
-      <tbody>
-        <tr>
-          <td bgcolor="#ff6600">
-            <Header msg="Hacker News Header"/>
-          </td>
-        </tr>
-        <tr style="height:10px"></tr>
-        <tr>
-          <td>
-            <table border="0" cellpadding="0" cellspacing="0" class="itemlist">
-            <Article v-for="(article, index) in articles" v-bind:key="index" :data="article" :index="index"></Article>
-            </table>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</center>
+  <center>
+    <div class="home">
+      <table border="0" cellpadding="0" cellspacing="0" width="85%" bgcolor="#f6f6ef">
+        <tbody>
+          <tr class="header-line">
+            <td bgcolor="#ff6600">
+              <Header/>
+            </td>
+          </tr>
+          <tr style="height:10px"></tr>
+          <tr>
+            <td>
+              <table border="0" cellpadding="0" cellspacing="0" class="itemlist">
+                <Article v-for="(article, index) in articles" v-bind:key="index" :data="article" :index="index"></Article>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </center>
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from '@/views/Header.vue'
-import Article from '@/views/Article.vue'
+import Header from '@/components/Home/Header.vue'
+import Article from '@/components/Home/Article.vue'
 
 export default {
   name: 'home',
@@ -63,44 +62,54 @@ export default {
 </script>
 <style lang="scss" scoped>
 
-td, th {
-    display: table-cell;
-    vertical-align: inherit;
+td,
+th {
+  display: table-cell;
+  vertical-align: inherit;
 }
+
 center {
-    display: block;
-    text-align: -webkit-center;
+  display: block;
+  text-align: -webkit-center;
 }
+
 tbody {
-    display: table-row-group;
-    vertical-align: middle;
-    border-color: inherit;
+  display: table-row-group;
+  vertical-align: middle;
+  border-color: inherit;
 }
+
 tr {
-    display: table-row;
-    vertical-align: inherit;
-    border-color: inherit;
+  display: table-row;
+  vertical-align: inherit;
+  border-color: inherit;
+
+  .header-info {
+    color: black;
+  }
 }
+
 table {
-    white-space: normal;
-    line-height: normal;
-    font-weight: normal;
-    font-size: medium;
-    font-style: normal;
-    color: -internal-quirk-inherit;
-    text-align: start;
-    font-variant: normal;
+  white-space: normal;
+  line-height: normal;
+  font-weight: normal;
+  font-size: medium;
+  font-style: normal;
+  color: -internal-quirk-inherit;
+  text-align: start;
+  font-variant: normal;
 }
-</style>
-<style>
+
 a:visited {
-    color: #828282;
-    text-decoration: none;
+  color: #828282;
+  text-decoration: none;
 }
+
 a:link {
-    color: #000000;
-    text-decoration: none;
+  color: #000000;
+  text-decoration: none;
 }
+
 </style>
 
 
