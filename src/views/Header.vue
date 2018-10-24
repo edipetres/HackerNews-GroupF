c207d4eec54120f77b5083f4366ce599d892c5c1
Emil Klausen <emilklausen@Emils-MacBook-Pro-2.local>
2018-10-24 11:43:52 +0200

    Create login page

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -4,7 +4,7 @@
           <tr>
             <td style="width:18px;padding-right:10px"><a href="#"><img src="/static/img/y18.gif" width="18px" height="18px" style="border:1px white solid;"></a></td>
             <td style="line-height:12pt; height:10px; text-align:left;"><b>Hacker News </b><a href="#">new</a> | <a href="#">comments</a> | <a href="#">show</a> | <a href="#">ask</a> | <a href="#">jobs</a>| <a href="#">submit</a></td>
-            <td style="text-align:right;padding-right:4px;">login</td>
+            <td style="text-align:right;padding-right:4px;"><router-link to="/login">Login</router-link></td>
           </tr>
       </tbody>
   </table>
