917642c8729ffe17b27177a77c5fc24f56ed83e0
Emil Klausen <emilklausen@Emils-MacBook-Pro-2.local>
2018-10-25 15:26:04 +0200

    added frontend and function to Comments and NewComments

--- src/views/NewComments.vue
+++ src/views/NewComments.vue
@@ -1,102 +1,64 @@
-
 <template>
-    <tr>
-      <td>
-        <table>
-          <tbody>
-            
-          </tbody>
-        </table>
-      </td>
-    </tr>
+  <center>
+    <div class="newcomments">
+      <table border="0" cellpadding="0" cellspacing="0" width="85%" bgcolor="#f6f6ef">
+        <tbody>
+          <tr class="header-line">
+            <td bgcolor="#ff6600">
+              <Header/>
+            </td>
+          </tr>
+          <tr style="height:10px"></tr>
+          <tr>
+            <td>
+              <table border="0" cellpadding="0" cellspacing="0" class="itemlist">
+                <!-- <Article v-for="(article, index) in articles" v-bind:key="index" :data="article" :index="index"></Article> -->
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </center>
 </template>
 
-<script>
-// @ is an alias to /src
-import Header from '@/components/Home/Header.vue'
 
+
+
+<script>
+import Header from "@/components/Home/Header.vue";
+import Comment from "@/components/Comments/Comment.vue";
 export default {
-  name: 'NewComments',
+  name: "newcomments",
   components: {
-    Header
+    Header,
+    Comment
   },
   data: function() {
     return {
-        
-    }
+      comments: []
+    };
   },
   created() {
-      
+    this.fetchComments();
   },
   methods: {
     fetchComments() {
-      const vm = this
-      this.$http.get('/newcomments')
-        .then(function (response) {
+      const vm = this;
+      this.$http
+        .get("/newcomments")
+        .then(function(response) {
           // handle success
-          vm.comments = response.data
+          vm.comments = response.data;
         })
-        .catch(function (error) {
+        .catch(function(error) {
           // handle error
-          alert('Error loading newcomments from server.', error)
+          alert("Error loading comments from server.", error);
         })
-        .then(function () {
+        .then(function() {
           // always executed
         });
     }
   }
-}
+};
 </script>
-<style lang="scss" scoped>
-
-td,
-th {
-  display: table-cell;
-  vertical-align: inherit;
-}
-
-center {
-  display: block;
-  text-align: -webkit-center;
-}
-
-tbody {
-  display: table-row-group;
-  vertical-align: middle;
-  border-color: inherit;
-}
-
-tr {
-  display: table-row;
-  vertical-align: inherit;
-  border-color: inherit;
-
-  .header-info {
-    color: black;
-  }
-}
-
-table {
-  white-space: normal;
-  line-height: normal;
-  font-weight: normal;
-  font-size: medium;
-  font-style: normal;
-  color: -internal-quirk-inherit;
-  text-align: start;
-  font-variant: normal;
-}
-
-a:visited {
-  color: #828282;
-  text-decoration: none;
-}
-
-a:link {
-  color: #000000;
-  text-decoration: none;
-}
-
-</style>
-
-
