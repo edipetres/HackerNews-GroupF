898397f0ea2df3f48ee657504edb5b0f6778591e
Nexao <emilklausen@Emils-MacBook-Pro-2.local>
2018-11-05 12:29:43 +0100

    Show when or wether user is logged in the header bar

--- src/components/Home/Header.vue
+++ src/components/Home/Header.vue
@@ -1,44 +1,39 @@
 <template>
-  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="padding:2px">
-                            <tbody>
-                                <tr>
-                                    <td style="width:18px;padding-right:4px">
-                                        <router-link to="/"><img src="/static/img/y18.gif" width="18" height="18" style="border:1px white solid;"></router-link>
-                                    </td>
-                                    <td style="line-height:12pt; height:10px;"><span class="pagetop"><b class="hnname"><router-link to="/">Hacker News</router-link></b>
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="padding:2px">
+    <tbody>
+        <tr>
+            <td style="width:18px;padding-right:4px">
+                <router-link to="/"><img src="/static/img/y18.gif" width="18" height="18" style="border:1px white solid;"></router-link>
+            </td>
+            <td style="line-height:12pt; height:10px;"><span class="pagetop"><b class="hnname"><router-link to="/">Hacker News</router-link></b>
               <router-link to="/">new</router-link>| <span class="pagetop"><router-link to="/newcomments">comments</router-link></span> | <a href="show">show</a> | <a href="ask">ask</a> | <a href="jobs">jobs</a> | <a href="submit">submit</a> </span>
-                                    </td>
-                                    <td style="text-align:right;padding-right:4px;"><span class="pagetop">
-                              <router-link to="/login">Login</router-link>
+            </td>
+            <td style="text-align:right;padding-right:4px;"><span class="pagetop">
+                              <a v-if="username">{{ username }}</a>
+                              <router-link to="/login" v-else>Login</router-link>
+                              
                           </span></td>
-                                </tr>
-
-      <!-- <tr>
-        
-        <td>
-          <a href="#"><img src="/static/img/y18.gif" class="icon"></a>
-        </td>
-        <td>
-          <span class="header-title">Hacker News</span>
-          <a href="#">new</a> |
-          <router-link to="/newcomments">comments</router-link> |
-          <a href="#">show</a> |
-          <a href="#">ask</a> |
-          <a href="#">jobs</a> |
-          <a href="#">submit</a>
-        </td>
-        <td style="text-align:right;padding-right:4px;">
-          <router-link to="/login">Login</router-link>
-        </td>
-      </tr> -->
+        </tr>
     </tbody>
-  </table>
-
+</table>
 </template>
 
 <script>
+import jwtDecode from 'jwt-decode'
 export default {
-  props: []
+  data() {
+        return {
+            username: '',
+            
+        };
+    },
+    props: [],
+    created() {
+      const token = localStorage.getItem('token')
+      if (token) {
+        const decodedToken = jwtDecode(token)
+        this.username = decodedToken.username
+      }
+    }
 };
 </script>
-
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,3 @@
-
 <template>
 <body>
     <b>Login</b>
@@ -18,91 +17,92 @@
             </tbody>
         </table>
         <br>
-    <br>
-        <input type="submit" value="login" @click="onLogin"> 
-    </div>
-    <br>
-    <br>
-    <a href="forgot"> Forgot your password?</a>
-    <br>
-    <br>
-    <b>Create Account</b>
-    <br>
-    <br>
-    <div>
-        <table border="0">
-            <tbody>
-                <tr>
-                    <td>username</td>
-                    <td><input type="text" v-model="user.name"></td>
-                </tr>
-                <tr>
-                    <td>password</td>
-                    <td><input type="password" v-model="user.password"></td>
-                </tr>
-            </tbody>
-        </table>
         <br>
-    <br>
-        <input type="submit" value="create account" @click="onCreate"> 
+        <input type="submit" value="login" @click="onLogin">
+    </div>
+        <br>
+        <br>
+        <a href="forgot"> Forgot your password?</a>
+        <br>
+        <br>
+        <b>Create Account</b>
+        <br>
+        <br>
+        <div>
+            <table border="0">
+                <tbody>
+                    <tr>
+                        <td>username</td>
+                        <td><input type="text" v-model="user.name"></td>
+                    </tr>
+                    <tr>
+                        <td>password</td>
+                        <td><input type="password" v-model="user.password"></td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            <br>
+            <input type="submit" value="create account" @click="onCreate">
         </div>
 </body>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      user: {
-        name: "",
-        password: ""
-      }
-    };
-  },
+    data() {
+        return {
+            user: {
+                name: "",
+                password: ""
+            }
+        };
+    },
 
-  methods: {
-    handleSubmit() {
-      // Send data to the server or update your stores and such.
-    },
-    onLogin() {
-      console.log("loggin in", this.user.nameLogin);
-      // Make a request for a user with a given ID
-      const axios = this.$http
-      const vm = this
-      axios
-        .post("/user/login", {
-          username: this.user.nameLogin,
-          password: this.user.passwordLogin
-        })
-        .then(function(response) {
-          console.log(response);
-          const token = response.data.token
-          
-          localStorage.setItem("token",token)
-          vm.$router.push("/")
-        })
-        .catch(function(error) {
-          console.log(error.response);
-        })
-        .then(function() {
-          // always executed
-        });
-    },
-    onCreate() {
-      const axios = this.$http
-      const vm = this
-      axios
-        .post("/user/registration", {
-          username: this.user.name,
-          password: this.user.password
-        })
-        .then(function(response) {
-          console.log(response);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
+    methods: {
+        handleSubmit() {
+            // Send data to the server or update your stores and such.
+        },
+        onLogin() {
+            console.log("loggin in", this.user.nameLogin);
+            // Make a request for a user with a given ID
+            const axios = this.$http
+            const vm = this
+            axios
+                .post("/user/login", {
+                    username: this.user.nameLogin,
+                    password: this.user.passwordLogin
+                })
+                .then(function (response) {
+                    const token = response.data.token
+
+                    localStorage.setItem("token", token)
+                    vm.$router.push("/")
+                })
+                .catch(function (error) {
+                    alert(error.response.data.error)
+                })
+                .then(function () {
+                    // always executed
+                });
+        },
+        onCreate() {
+            const axios = this.$http
+            const vm = this
+            axios
+                .post("/user/registration", {
+                    username: this.user.name,
+                    password: this.user.password
+                })
+                .then(function (response) {
+                    console.log(response);
+                    // TODO
+                    // alert that new user registration was successful prompting them to log in
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
     }
-  }
 };
 </script>
